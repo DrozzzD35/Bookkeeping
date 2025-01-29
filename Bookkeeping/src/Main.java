0952dad1fbee1664740329c422f96ba325cfd5a8
Nikita Kapustin <kapustin3595@yandex.ru>
2025-01-29 22:17:13 +0300

    feat: добавлены файлы для проекта

--- Bookkeeping/src/Main.java
+++ Bookkeeping/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+
+
+    /**
+
+     Классы: массивы(MRecord, YRecord), бизнес логика, enum{месяц, год}, месячный отчет, годовой отчет
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
     }
 }
