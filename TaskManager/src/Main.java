943dc076461e1bb827fd1d69f87a5eb202caacad
Nikita Kapustin <kapustin3595@yandex.ru>
2025-04-13 21:19:57 +0300

    добавлено несколько функций в меню

--- TaskManager/src/Main.java
+++ TaskManager/src/Main.java
@@ -14,74 +14,129 @@ public class Main {
 
             switch (answerUser) {
                 case 1: {
-                    System.out.println("Название задачи: ");
-                    String name = scanner.next();
+                    System.out.println("1. Создать задачу");
+                    System.out.println("2. Создать большую задачу");
+                    System.out.println("3. Создать подзадачу");
+                    int answer = scanner.nextInt();
 
-                    System.out.println("Описание задачи: ");
-                    String description = scanner.next();
+                    if (answer == 1) {
+                        System.out.println("Название задачи: ");
+                        String name = scanner.next();
 
-                    manager.crateTask(name, description);
-                    System.out.println("Задачи создана успешно");
+                        System.out.println("Описание задачи: ");
+                        String description = scanner.next();
+
+                        manager.crateTask(name, description);
+                        System.out.println("Задачи создана успешно");
+                        System.out.println();
+                    } else if (answer == 2) {
+                        System.out.println("Название задачи: ");
+                        String name = scanner.next();
+
+                        System.out.println("Описание задачи: ");
+                        String description = scanner.next();
+
+                        manager.crateEpic(name, description);
+                        System.out.println("Большая задачи создана успешно");
+                        System.out.println();
+                    } else if (answer == 3) {
+                        System.out.println("Название задачи: ");
+                        String name = scanner.next();
+
+                        System.out.println("Описание задачи: ");
+                        String description = scanner.next();
+
+                        manager.crateSubTask(name, description);
+                        System.out.println("Подзадачи создана успешно");
+                        System.out.println();
+                    } else {
+                        System.out.println("Неверно указана команда");
+                    }
+
                     break;
-
-                }
-                case 2: {
-                    System.out.println("Название задачи: ");
-                    String name = scanner.next();
-
-                    System.out.println("Описание задачи: ");
-                    String description = scanner.next();
-
-                    manager.crateEpic(name, description);
-                    System.out.println("Задачи создана успешно");
-
-                    break;
-
-
                 }
                 case 3: {
                     System.out.println("Введите идентификатор задачи для её удаления");
                     int id = scanner.nextInt();
                     manager.removeTaskById(id);
+                    System.out.println();
                     break;
-
-
                 }
                 case 4: {
                     System.out.println("Вы уверены что хотите удалить все задачи?");
-                    String answer = scanner.nextLine();
-                    if (answer == "да") {
+                    System.out.println("1. Да");
+                    System.out.println("2. Нет");
+                    int answer = scanner.nextInt();
+                    if (answer == 1) {
                         manager.removeAllTask();
                         System.out.println("Все задачи удалены");
+
+                    } else {
+                        System.out.println("Задачи не удалены");
                     }
+                    System.out.println();
+
                     break;
-
-
                 }
                 case 5: {
                     System.out.println("Введите идентификатор задачи для её поиска");
                     int id = scanner.nextInt();
-                    Task task = manager.findTask(id);
-                    System.out.println("Найдена задача " + task.getName() + ". ");
-                    System.out.println("Идентификатор задачи: " + task.getId());
-                    System.out.println("Описание: " + task.getDescription());
-                    System.out.println("Статус: " + task.getStatus());
-                    System.out.println("Тип: " + task.getType());
+                    manager.printTask(id);
                     System.out.println();
                     break;
-
-
                 }
                 case 6: {
                     manager.printAllTasks();
                     break;
 
                 }
-                default: {
-                    System.out.println("Введена неверная команда");
+                case 7: {
+
+                    System.out.println("Введите идентификатор задачи: ");
+                    int id = scanner.nextInt();
+                    Task task = manager.findTask(id);
+                    manager.printTask(id);
+
+                    System.out.println("1. Изменить имя задачи");
+                    System.out.println("2. Изменить описание задачи");
+                    System.out.println("3. Изменить статус задачи");
+                    int answer = scanner.nextInt();
+
+
+                    if (answer == 1) {
+                        System.out.println("Какое имя вы хотели бы присвоить");
+                        String name = scanner.next();
+                        manager.updateName(task, name);
+                        System.out.println("Имя задачи успешно изменено на " + name);
+                        System.out.println();
+                        manager.printTask(id);
+                        break;
+                    } else if (answer == 2) {
+                        System.out.println("Какое описание вы хотели бы присвоить");
+                        String description = scanner.next();
+                        manager.updateDescription(task, description);
+                        System.out.println("Описание задачи успешно изменено на " + description);
+                        System.out.println();
+                        manager.printTask(id);
+                    } else if (answer == 3) {
+                        System.out.println("Задача " + task.getName() + " имеет статус " + task.getStatus());
+                        manager.changeStatus(id);
+                    } else {
+                        System.out.println("Введена неверная команда");
+                    }
+                    break;
+                }
+                case 8: {
                     return;
                 }
+                default: {
+                    System.out.println("Введена неверная команда");
+                    System.out.println();
+                    break;
+                }
             }
+
+
         }
 
     }
@@ -90,11 +145,12 @@ public class Main {
     public static void printMenu() {
         System.out.println("Введите команду (1-6): ");
         System.out.println("1. Создать задачу");
-        System.out.println("2. Создать большую задачу");
-        System.out.println("3. Удалить задачу по id");
+        System.out.println("3. Удалить задачу по идентификатору");
         System.out.println("4. Удалить все задача");
         System.out.println("5. Найти задачу");
         System.out.println("6. Получение списка всех задач");
+        System.out.println("7. Изменить имя, описание или статус задачи");
+        System.out.println("8. Выход");
         System.out.println();
     }
 
--- TaskManager/src/model/Epic.java
+++ TaskManager/src/model/Epic.java
@@ -2,16 +2,14 @@ package model;
 
 import utils.Identity;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Epic extends Task {
 
 
-    public Epic(String name, String description) {
-        super(name, description);
+    public Epic(String nameEpic, String descriptionEpic) {
+        super(nameEpic, descriptionEpic);
         this.id = Identity.INSTANCE.generateId() - 1;
         this.type = Type.EPIC;
         this.status = Status.NEW;
+//        SubTask subTask = new SubTask(nameSubTask, descriptionSubTask);
     }
 }
